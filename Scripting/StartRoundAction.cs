b7d3b094dfa662eb200760a748bfbe587a6cb95b
LindseyK2 <lindseyk2@byui.edu>
2021-11-28 20:25:00 -0700

    Touched up Start Round Action

--- Scripting/StartRoundAction.cs
+++ Scripting/StartRoundAction.cs
@@ -10,30 +10,44 @@ namespace Final_Project.Scripting
     {
         public override void Execute(Dictionary<string, List<Actor>> cast)
         {
+            List<Actor> playerCards = cast["PlayerCards"];
+            List<Actor> npcCards = cast["NPCCards"];
+            List<Actor> Card = cast["Cards"];
             CardDeck cardDeck = new CardDeck();
-
-            int x1 = 200;
-            for (int i = 0; i < 6; i++)
+            int count = 0;
+            
+            foreach (Actor playerCard in playerCards)
             {
-                Point userHand = new Point(x1, 520);
-                Card card = new Card(userHand);
-                cast["PlayerCards"].Add(card);
-                x1 += 65;
+                count += 1;
             }
-
-            int x2 = 200;
-            for (int i = 0; i < 6; i = i + 1)
+            foreach (Actor npcCard in npcCards)
             {
-                Point userHand = new Point(x2, 10);
-                Card card = new Card(userHand);
-                cast["NPCCards"].Add(card);
-                x2 += 65;
+                count += 1;
             }
+            if (count == 0)
+            {
+                int x1 = 200;
+                for (int i = 0; i < 6; i++)
+                {
+                    Point userHand = new Point(x1, 520);
+                    Card card = new Card(userHand);
+                    cast["PlayerCards"].Add(card);
+                    x1 += 65;
+                }
 
-            Point faceUpCardPosition = new Point(Constants.DECK_X, Constants.DECK_Y);
-            Card faceUpCard = new Card(faceUpCardPosition);
-            cast["Cards"].Add(faceUpCard);
+                int x2 = 200;
+                for (int i = 0; i < 6; i = i + 1)
+                {
+                    Point userHand = new Point(x2, 10);
+                    Card card = new Card(userHand);
+                    cast["NPCCards"].Add(card);
+                    x2 += 65;
+                }
 
+                Point faceUpCardPosition = new Point(Constants.DECK_X, Constants.DECK_Y);
+                Card faceUpCard = new Card(faceUpCardPosition);
+                cast["Cards"].Add(faceUpCard);
+            }
         }
     }
 }
